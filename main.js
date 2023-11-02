d0f9f8b324b94357bd847fa46e5ff97404ed1e03
TaiSonRZR <77694156+TaiSonRZR@users.noreply.github.com>
2023-11-02 16:21:04 +0100

    Set up Backend installing Frameworks

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import './style.css'
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log('App Listening on port ${port}');
+});
