cfd7f8fa1d9ef4e61cc4a9e83085434f6b098aa4
TaiSonRZR <77694156+TaiSonRZR@users.noreply.github.com>
2023-11-07 23:05:02 +0100

    added gameboard to place objects

--- public/index.js
+++ public/index.js
@@ -12,18 +12,17 @@ function hideAllDivs() {
   }
 }
 
-function hideActiveDiv(){
-  if(document.getElementById("home").style.display === "block"){
-    document.getElementById("home").style.display = "none"
-  } 
-  else if(document.getElementById("headquarters").style.display === "block"){
-    document.getElementById("headquarters").style.display = "none"
-  } 
-  else if(document.getElementById("shop").style.display === "block"){
-    document.getElementById("shop").style.display = "none"
-  } 
-  else if(document.getElementById("options").style.display === "block"){
-    document.getElementById("options").style.display = "none"
+function hideActiveDiv() {
+  if (document.getElementById("home").style.display === "block") {
+    document.getElementById("home").style.display = "none";
+  } else if (
+    document.getElementById("headquarters").style.display === "block"
+  ) {
+    document.getElementById("headquarters").style.display = "none";
+  } else if (document.getElementById("shop").style.display === "block") {
+    document.getElementById("shop").style.display = "none";
+  } else if (document.getElementById("options").style.display === "block") {
+    document.getElementById("options").style.display = "none";
   }
 }
 
@@ -46,3 +45,34 @@ document.getElementById("options-button").addEventListener("click", () => {
   hideActiveDiv();
   document.getElementById("options").style.display = "block";
 });
+
+// HEADQUARTERS
+const optionContainer = document.querySelector(".option-container");
+const flipButton = document.querySelector("#flip-button");
+const gamesBoardContainer = document.querySelector("#gamesboard-container");
+//================================
+//ROTATE FUNCTION IN SHIPS CONTAINER
+let angle = 0;
+function flip() {
+  const optionShips = Array.from(optionContainer.children);
+
+  angle = angle === 0 ? 90 : 0;
+
+  optionShips.forEach(
+    (optionShip) => (optionShip.style.transform = `rotate(${angle}deg)`)
+  );
+}
+
+//CREATE BOARDS
+const width = 10;
+
+function createBoard() {
+  const gameBoardContainer = document.createElement("div");
+  gameBoardContainer.classList.add("game-board");
+  gameBoardContainer.style.backgroundColor = "pink";
+
+  gamesBoardContainer.append(gameBoardContainer);
+}
+
+createBoard();
+flipButton.addEventListener("click", flip);
